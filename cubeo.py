0499233a4e6e7bbb1d07f278e628107c4939006c
mattvlaw <mattvlaw@gmail.com>
2017-05-04 01:43:29 -0400

    first commit

---
+++ cubeo.py
@@ -0,0 +1,74 @@
+from maestro import Controller
+import time
+
+class Cubeo:
+	def __init__(self, port='dummy'):
+		self.port = port
+		self.servos = [2,4,3,5]
+		self.open_controller(self.port)
+		
+		self.SLOW = 6200
+		self.MEDIUM = 6600
+		self.FAST = 7000
+		self.VERYFAST= 7500
+		self.STOP = 0
+		self.running = False
+		self.speed = self.SLOW
+
+
+	def open_controller(self,port):
+		if port=="dummy":
+			self.cubeo_controller = dummyController()
+		else:
+			self.cubeo_controller  = Controller(ttyStr=port)
+			for servo in self.servos:
+				self.cubeo_controller.setAccel(servo,1)
+
+	def close_controller(self):
+		self.cubeo_controller.close()
+
+	def do_the_wave(self,start=0,direction="clockwise"):
+		for servo in self.servos:
+			self.cubeo_controller.setTarget(servo,self.SLOW)
+			print("servo: ",servo)
+			time.sleep(1)
+			self.cubeo_controller.setTarget(servo,self.STOP)
+	def expand_all(self):
+		for servo in self.servos:
+			self.cubeo_controller.setTarget(servo,self.SLOW)
+			print("servo: ",servo)
+		time.sleep(5)
+		for servo in self.servos:
+			self.cubeo_controller.setTarget(servo,self.STOP)
+	def breathe_slow(self):
+		self.set_all_speed(self.SLOW)
+	
+	def breathe_medium(self):
+		self.set_all_speed(self.MEDIUM)
+
+	def breathe_fast(self):
+		self.set_all_speed(self.FAST)
+	
+	def breathe_very_fast(self):
+		self.set_all_speed(self.VERYFAST)
+
+	def stop_breathing(self):
+		self.set_all_speed(0)
+
+	
+	def set_all_speed(self,speed):
+		for servo in self.servos:
+			self.cubeo_controller.setTarget(servo,speed)
+			print("servo: ",servo)
+	
+	def multiple_waves(self,num_iterations = 1):
+		for i in range(num_iterations):
+			do_the_wave()
+class dummyController:
+	def __init__(self):
+		print "started dummy controller"
+	def setTarget(self,channel,target):
+		print "setting target for channel"+str(channel)+" to "+str(target)
+	def close(self):
+		print "closing dummy controller"
+
---
+++ cubeoserver.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from cubeo import Cubeo
+
+
+control_panel = '''<html>
+						<body>
+						<h3>%s</h3>
+						<ul>
+						<li><a href="/wave">Do the Wave</a></li>
+						<li><a href="/breatheslow">Breathe Slowly</a></li>
+						<li><a href="/breathemedium">Breathe Normally</a></li>
+						<li><a href="/breathefast">Breath Fast</a></li>
+						<li><a href="/breatheveryfast">Breathe VERY FAST</a></li>
+						<a href="/breathestop">Stop Breathing</a></li>
+						</ul>
+						</body>
+					<html>
+'''
+
+app = Flask(__name__)
+cubeo = Cubeo("COM12")
+
+@app.route("/")
+def hello():
+	return control_panel%"Welcome, choose a command:"
+
+@app.route("/wave")
+def wave():
+	cubeo.do_the_wave()
+	return control_panel%"Doing the wave. Choose another command:"
+
+@app.route("/breathefast")
+def breathefast():
+	cubeo.breathe_fast()
+	return control_panel%"Breathing fast. Choose another command:"
+
+@app.route("/breatheslow")
+def breatheslow():
+	cubeo.breathe_slow()
+	return control_panel%"Breathing slowly. Choose another command:"
+
+@app.route("/breathestop")
+def breathestop():
+	cubeo.stop_breathing()
+	return control_panel%"Stopped breathing. Choose another command:"
+
+@app.route("/breathemedium")
+def breathemedium():
+	cubeo.breathe_medium()
+	return control_panel%"Breathing. Choose another command:"
+
+@app.route("/breatheveryfast")
+def breatheveryfast():
+	cubeo.breathe_very_fast()
+	return control_panel%"HYPERVENTILATING!!!!. PLEASE choose another command:"
+
+
+
+if __name__ == "__main__":
+	app.run()
